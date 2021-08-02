d24fc3ae6423a9fa468b051e7a89a45db87313bb
Nathaniel Cuenca <ncuenca.nac@gmail.com>
2021-08-02 15:19:58 -0700

    doing stuff

--- models/user.js
+++ models/user.js
@@ -1,5 +1,9 @@
 "use strict";
 
+const bcrypt = require('bcrypt');
+const { BCRYPT_WORK_FACTOR } = require('../config');
+const db = require('../db');
+
 /** User of the site. */
 
 class User {
@@ -9,22 +13,70 @@ class User {
    */
 
   static async register({ username, password, first_name, last_name, phone }) {
+    const hashedPassword = await bcrypt.hash(password, BCRYPT_WORK_FACTOR);
+
+    const result = await db.query(
+      `INSERT INTO users (username,
+                             password,
+                             first_name,
+                             last_name,
+                             phone)
+         VALUES
+           ($1, $2, $3, $4, $5)
+         RETURNING username, password, first_name, last_name, phone`,
+    [username, hashedPassword, first_name, last_name, phone]);
+
+    return result.rows[0];
   }
 
   /** Authenticate: is username/password valid? Returns boolean. */
 
   static async authenticate(username, password) {
+    const hashedPassword = await bcrypt.hash(password, BCRYPT_WORK_FACTOR);
+
+    const result = await db.query(
+      `SELECT password
+       FROM users
+       WHERE username = $1`,
+       [username]);
+    const userPw = result.rows[0];
+    
+    if (userPw) {
+      return await bcrypt.compare(userPw, hashedPassword) === true
+    }
+    
+    throw new NotFoundError(`Invalid login`);
   }
 
   /** Update last_login_at for user */
 
   static async updateLoginTimestamp(username) {
+    const result = await db.query(
+          `UPDATE users
+           SET last_login_at = current_timestamp
+             WHERE username = $1
+             RETURNING username, last_login_at`,
+        [username]);
+    const user = result.rows[0];
+
+    if (!user) throw new NotFoundError(`Invalid username`);
+
+    return user;
   }
 
   /** All: basic info on all users:
    * [{username, first_name, last_name}, ...] */
 
   static async all() {
+    const results = await db.query(
+      `SELECT username, first_name, last_name
+       FROM users`
+    )
+    const users = results.rows;
+
+    if (!users) throw new NotFoundError(`No users`);
+
+    return users;
   }
 
   /** Get: get user by username
@@ -37,6 +89,16 @@ class User {
    *          last_login_at } */
 
   static async get(username) {
+    const result = await db.query(
+      `SELECT username, first_name, last_name, phone, join_at, last_login_at
+       FROM users
+       WHERE username = $1`,
+       [username]);
+    const user = result.rows[0];
+
+    if (!user) throw new NotFoundError(`No such user: ${username}`);
+
+    return user;
   }
 
   /** Return messages from this user.
@@ -48,6 +110,38 @@ class User {
    */
 
   static async messagesFrom(username) {
+    const results = await db.query(
+          `SELECT m.id,
+                  m.from_username,
+                  m.to_username,
+                  t.first_name AS to_first_name,
+                  t.last_name AS to_last_name,
+                  t.phone AS to_phone,
+                  m.body,
+                  m.sent_at,
+                  m.read_at
+             FROM messages AS m
+                    JOIN users AS f ON m.from_username = f.username
+                    JOIN users AS t ON m.to_username = t.username
+             WHERE m.from_username = $1`,
+        [username]);
+
+    let messages = results.rows;
+
+    if (!messages) throw new NotFoundError(`No such message: ${id}`);
+
+    return {
+      id: messages.id,
+      to_user: {
+        username: messages.to_username,
+        first_name: messages.to_first_name,
+        last_name: messages.to_last_name,
+        phone: messages.to_phone,
+      },
+      body: messages.body,
+      sent_at: messages.sent_at,
+      read_at: messages.read_at,
+    };
   }
 
   /** Return messages to this user.
@@ -59,6 +153,38 @@ class User {
    */
 
   static async messagesTo(username) {
+    const results = await db.query(
+          `SELECT m.id,
+                  m.from_username,
+                  m.to_username,
+                  f.first_name AS from_first_name,
+                  f.last_name AS from_last_name,
+                  f.phone AS from_phone,
+                  m.body,
+                  m.sent_at,
+                  m.read_at
+             FROM messages AS m
+                    JOIN users AS f ON m.from_username = f.username
+                    JOIN users AS t ON m.to_username = t.username
+             WHERE m.to_username = $1`,
+        [username]);
+
+    let messages = results.rows;
+
+    if (!messages) throw new NotFoundError(`No such message: ${id}`);
+
+    return {
+      id: messages.id,
+      from_user: {
+        username: messages.from_username,
+        first_name: messages.from_first_name,
+        last_name: messages.from_last_name,
+        phone: messages.from_phone,
+      },
+      body: messages.body,
+      sent_at: messages.sent_at,
+      read_at: messages.read_at,
+    };
   }
 }
 
