19be72567ab60aae5a8b28df6255c1703362996e
stocktons <sarah@thanksforallthe.fish>
2021-08-03 13:14:15 -0400

    users routes

--- routes/auth.js
+++ routes/auth.js
@@ -9,19 +9,28 @@ const { BadRequestError } = require("../expressError");
 
 /** POST /login: {username, password} => {token} */
 router.post("/login", async function (req, res, next) {
-    const { username, password } = req.body;
-    if (await User.authenticate(username, password) !== true) {
-        throw new BadRequestError('INVALID LOGIN');
-    }
-    let token = jwt.sign({ username }, SECRET_KEY)
-    User.updateLoginTimestamp(username);
-    return res.json({ token });
-  });
+  const { username, password } = req.body;
+  if (await User.authenticate(username, password) !== true) {
+    throw new BadRequestError('INVALID LOGIN');
+  }
+  let token = jwt.sign({ username }, SECRET_KEY)
+  User.updateLoginTimestamp(username);
+  return res.json({ token });
+});
 
 
 /** POST /register: registers, logs in, and returns token.
  *
  * {username, password, first_name, last_name, phone} => {token}.
  */
+router.post("/register", async function (req, res, next) {
+  const { username, password, first_name, last_name, phone } = req.body;
+  if (await User.register(username, password, first_name, last_name, phone)) {
+    throw new BadRequestError('Please choose a different username.');
+  }
+  let token = jwt.sign({ username }, SECRET_KEY)
+  User.updateLoginTimestamp(username);
+  return res.json({ token });
+});
 
 module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -3,19 +3,27 @@
 const Router = require("express").Router;
 const router = new Router();
 
+const { ensureLoggedIn, ensureCorrectUser } = require("../middleware/auth");
+
 
 /** GET / - get list of users.
  *
  * => {users: [{username, first_name, last_name, phone}, ...]}
  *
  **/
+router.get("/", ensureLoggedIn, function (req, res, next) {
+    return res.json({ users: await Users.all });
+})
 
 
-/** GET /:username - get detail of users.
+/** GET /:username - get detail of user.
  *
  * => {user: {username, first_name, last_name, phone, join_at, last_login_at}}
  *
  **/
+router.get("/:username", ensureLoggedIn, ensureCorrectUser, function (req, res, next) {
+    return res.json({ user: await Users.get(username) });
+})
 
 
 /** GET /:username/to - get messages to user
@@ -27,7 +35,9 @@ const router = new Router();
  *                 from_user: {username, first_name, last_name, phone}}, ...]}
  *
  **/
-
+router.get("/:username/to", ensureLoggedIn, ensureCorrectUser, function (req, res, next) {
+    return res.json({ user: await Users.messagesTo(username) });
+})
 
 /** GET /:username/from - get messages from user
  *
@@ -38,5 +48,9 @@ const router = new Router();
  *                 to_user: {username, first_name, last_name, phone}}, ...]}
  *
  **/
+
+router.get("/:username/to", ensureLoggedIn, ensureCorrectUser, function (req, res, next) {
+    return res.json({ user: await Users.messagesFrom(username) });
+})
 
 module.exports = router;
